fe8ae781bd1022cda25c623a894115409f70e9d2
Truong Chi Nhan <nhanbin03@gmail.com>
2023-11-21 14:25:55 +0700

    Add Animation class

---
+++ src/gui/Animation.cpp
@@ -0,0 +1,101 @@
+#include "Animation.h"
+
+Animation::Animation()
+: mSpriteSheet(Texture2D())
+, mFrameSize(Vector2())
+, mNumFrames(0)
+, mCurrentFrame(0)
+, mDuration(0)
+, mElapsedTime(0)
+, mRepeated(false) {
+}
+
+void Animation::update(float dt) {
+    float timePerFrame = mDuration / mNumFrames;
+    mElapsedTime += dt;
+
+    while (mElapsedTime >= timePerFrame && (mCurrentFrame <= mNumFrames || mRepeated)) {
+        mElapsedTime -= timePerFrame;
+
+        if (mFrameGridPosition.x + 1 < mSpriteSheetGridSize.x) {
+            mFrameGridPosition.x++;
+        } else {
+            mFrameGridPosition.x = 0;
+            mFrameGridPosition.y = (int)(mFrameGridPosition.y + 1) % (int)mSpriteSheetGridSize.y;
+        }
+
+        if (mRepeated) {
+            mCurrentFrame = (mCurrentFrame + 1) % mNumFrames;
+        } else {
+            mCurrentFrame++;
+        }
+
+    }
+}
+
+void Animation::draw(Vector2 position) {
+    Rectangle sourceRect = {
+        mFrameGridPosition.x * mFrameSize.x,
+        mFrameGridPosition.y * mFrameSize.y,
+        mFrameSize.x,
+        mFrameSize.y
+    };
+
+    DrawTextureRec(mSpriteSheet, sourceRect, position, WHITE);
+}
+
+void Animation::setSpriteSheet(const Texture2D& spriteSheet) {
+    mSpriteSheet = spriteSheet;
+}
+
+Texture2D Animation::getSpriteSheet() const {
+    return mSpriteSheet;
+}
+
+void Animation::setSpriteSheetGridSize(const Vector2& spriteSheetGridSize) {
+    mSpriteSheetGridSize = spriteSheetGridSize;
+}
+
+Vector2 Animation::getSpriteSheetGridSize() const {
+    return mSpriteSheetGridSize;
+}
+
+void Animation::setFrameSize(const Vector2& frameSize) {
+    mFrameSize = frameSize;
+}
+
+Vector2 Animation::getFrameSize() const {
+    return mFrameSize;
+}
+
+void Animation::setNumFrames(int numFrames) {
+    mNumFrames = numFrames;
+}
+
+int Animation::getNumFrames() const {
+    return mNumFrames;
+}
+
+void Animation::setDuration(float duration) {
+    mDuration = duration;
+}
+
+float Animation::getDuration() const {
+    return mDuration;
+}
+
+void Animation::setRepeating(bool flag) {
+    mRepeated = flag;
+}
+
+bool Animation::isRepeating() const {
+    return mRepeated;
+}
+
+void Animation::restart() {
+    mCurrentFrame = 0;
+}
+
+bool Animation::isFinished() const {
+    return mCurrentFrame >= mNumFrames;
+}
---
+++ src/gui/Animation.h
@@ -0,0 +1,49 @@
+#ifndef SRC_GUI_ANIMATION_H
+#define SRC_GUI_ANIMATION_H
+
+#include "raylib.h"
+
+#include <string>
+#include <vector>
+
+class Animation {
+public:
+    Animation();
+    
+    void update(float dt);
+    void draw(Vector2 position);
+
+    void setSpriteSheet(const Texture2D& spriteSheet);
+    Texture2D getSpriteSheet() const;
+
+    void setSpriteSheetGridSize(const Vector2& spriteSheetGridSize);
+    Vector2 getSpriteSheetGridSize() const;
+
+    void setFrameSize(const Vector2& frameSize);
+    Vector2 getFrameSize() const;
+
+    void setNumFrames(int numFrames);
+    int getNumFrames() const;
+
+    void setDuration(float duration);
+    float getDuration() const;
+
+    void setRepeating(bool flag);
+    bool isRepeating() const;
+
+    void restart();
+    bool isFinished() const;
+
+private:
+    Texture2D mSpriteSheet;
+    Vector2 mSpriteSheetGridSize;
+    Vector2 mFrameSize;
+    Vector2 mFrameGridPosition;
+    int mNumFrames;
+    int mCurrentFrame;
+    float mDuration;
+    float mElapsedTime;
+    bool mRepeated;
+};
+
+#endif // SRC_GUI_ANIMATION_H
